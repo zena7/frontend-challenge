058ee9cbf463fea8b564e5d3e57a30d6b02d1b66
Nataly Zernova <nz_18@mail.ru>
2024-01-31 18:34:25 +0300

    feat: 🎸 add layout component

---
+++ src/layout/Layout.mjs
@@ -0,0 +1,11 @@
+import * as classes from './Layout.module.css';
+
+export function Layout() {
+  return (
+    <>
+      <header className={classes.header}>Baby</header>
+      {/* <main></main>
+      <footer></footer> */}
+    </>
+  );
+}
