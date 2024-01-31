3c1c1c081b3eb7e10d1d0bcdb26b1eb994d1ff4c
Nataly Zernova <nz_18@mail.ru>
2024-01-31 18:42:21 +0300

    refactor: 💡 create list in  header

--- src/layout/Layout.mjs
+++ src/layout/Layout.mjs
@@ -3,7 +3,12 @@ import * as classes from './Layout.module.css';
 export function Layout() {
   return (
     <>
-      <header className={classes.header}>Baby</header>
+      <header className={classes.header}>
+        <ul>
+          <li>Все котики</li>
+          <li>Любимые котики</li>
+        </ul>
+      </header>
       {/* <main></main>
       <footer></footer> */}
     </>
