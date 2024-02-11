f0aac6493d9c2c865e50bf2698048f153b9b31c0
Nataly Zernova <nz_18@mail.ru>
2024-02-11 19:53:07 +0300

    feat: 🎸 setup routes

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,45 +1,17 @@
-import { Button, List } from '../shared/ui';
-import { useFetch } from '../shared/use-fetch';
-import * as classes from './Layout.module.css';
+// import * as classes from './Layout.module.css';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from '../header/Header';
+import { FavoriteCatsPage } from '../pages/favoriteCatsPage';
+import { MainPage } from '../pages/mainPage';
 
 export function Layout() {
-  const { data = [] } = useFetch('/images/search?limit=15');
-
-  console.log('data---', data);
-
   return (
     <>
-      <header>
-        <nav>
-          <List className={classes.mainNavigation}>
-            <li>
-              <a href="№">Все котики</a>
-            </li>
-            <li>
-              <a href="#">Любимые котики</a>
-            </li>
-          </List>
-        </nav>
-      </header>
-      <main>
-        <List className="catsList">
-          {data?.map((item) => {
-            return (
-              <li className="catItem">
-                <div className="catContainer">
-                  <img
-                    key={item.id}
-                    src={item.url}
-                    alt="cat"
-                    className="catImg"
-                  />
-                </div>
-              </li>
-            );
-          })}
-        </List>
-        <Button className="buttonDownload">Загружаем еще котиков</Button>
-      </main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/favoriteCats" element={<FavoriteCatsPage />} />
+      </Routes>
     </>
   );
 }
