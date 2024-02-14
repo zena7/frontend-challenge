59f1ffb025d9cfc2b8bcf6c9e304078865c691d4
Nataly Zernova <nz_18@mail.ru>
2024-02-14 18:15:39 +0300

    refactor: 💡 add Outlet for pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import { FavoriteCatsPage } from './pages/favoriteCatsPage';
+import { MainPage } from './pages/mainPage';
 import { Layout } from './layout/Layout';
 
 export function App() {
-  return <Layout />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="favoriteCats" element={<FavoriteCatsPage />} />
+      </Route>
+    </Routes>
+  );
 }
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,17 +1,14 @@
 // import * as classes from './Layout.module.css';
-import { Route, Routes } from 'react-router-dom';
+
+import { Outlet } from 'react-router-dom';
 import { Header } from '../header/Header';
-import { FavoriteCatsPage } from '../pages/favoriteCatsPage';
-import { MainPage } from '../pages/mainPage';
 
 export function Layout() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/favoriteCats" element={<FavoriteCatsPage />} />
-      </Routes>
+      <Outlet />
+      <footer> 2024 year</footer>
     </>
   );
 }
