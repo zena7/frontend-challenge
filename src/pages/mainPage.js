ce1fc0223adca2f0f501abf1c44f4adc811b5e1e
Nataly Zernova <nz_18@mail.ru>
2024-02-15 20:27:53 +0300

    feat: 🎸 add logic for downloading cats from api

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,36 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { CatCard } from '../catCard/CatCard';
 import { Button, List } from '../shared/ui';
 import { useFetch } from '../shared/use-fetch';
 
 export function MainPage() {
   const [page, setPage] = useState(0);
+  const [cats, setCats] = useState([]);
   const { data = [] } = useFetch(
     `/images/search?limit=15&page=${page}&order=ASC`,
   );
 
-  console.log('data---', 'page', page, data, localStorage.getItem('catsLocal'));
+  useEffect(() => {
+    if (data) {
+      setCats((prev) => prev.concat(data));
+      console.log('CATS', cats);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  console.log('Main page');
 
   function handleClick() {
-    console.log('click and page - ', page);
-
     setPage((prev) => prev + 1);
+    console.log('click and page - ', page);
   }
 
   return (
     <main>
       <List className="catsList">
-        {/* {(JSON.parse(localStorage.getItem('catsLocal')) ??  */}
-        {data?.map((item) => {
+        {cats?.map((item) => {
           return (
             <li className="catItem" key={item.id}>
-              <div className="catContainer">
-                <img
-                  // key={item.id}
-                  src={item.url}
-                  alt="cat"
-                  className="catImg"
-                />
-              </div>
+              <CatCard url={item.url} />
             </li>
           );
         })}
--- src/shared/use-fetch.js
+++ src/shared/use-fetch.js
@@ -4,21 +4,15 @@ import { get } from './api';
 export function useFetch(url) {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState(null);
-  // (() => {
-  //   const cachedData = localStorage.getItem('catsLocal');
-  //   return cachedData ? JSON.parse(cachedData) : null;
-  // });
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    // if (!data) {
     get(url)
       .then((response) => response.json())
       .then((result) => {
         setLoading(false);
-        console.log('result', result);
+        console.log('result from useEffect: ', result);
         setData(result);
-        // localStorage.setItem('catsLocal', JSON.stringify(result));
       })
       .catch((err) => {
         setLoading(false);
