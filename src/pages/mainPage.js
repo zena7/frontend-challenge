a1d7023371c313563bc895ffab5fd944d5904029
Nataly Zernova <nz_18@mail.ru>
2024-02-19 20:37:30 +0300

    feat: 🎸 add inifinite scroll and downlowd showing cats carts

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,44 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { CatCard } from '../catCard/CatCard';
 import { List } from '../shared/ui';
 import { useFetch } from '../shared/use-fetch';
 
-export function MainPage() {
+export function MainPage({ getPage, pageIs, getData, dataIs }) {
   const [page, setPage] = useState(0);
   const [cats, setCats] = useState([]);
   const { data = [], loading } = useFetch(
-    `/images/search?limit=15&page=${page}&order=ASC`,
+    `/images/search?limit=15&page=${pageIs > 0 && page === 0 ? pageIs : page}&order=ASC`,
   );
 
   useEffect(() => {
-    if (data) {
+    if (dataIs.length > 0 && cats.length === 0) {
+      setCats(dataIs);
+      setPage(pageIs);
+    } else if (data) {
       setCats((prev) => prev.concat(data));
-      console.log('CATS', cats);
+      getData(cats);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
-  function handlerScroll(event) {
-    if (
-      event.target.documentElement.scrollHeight -
-        (event.target.documentElement.scrollTop + window.innerHeight) <
-      60
-    ) {
-      console.log('scroll');
-      setPage((prev) => prev + 1);
-    }
-  }
+  const handlerScroll = useCallback(
+    (event) => {
+      if (
+        event.target.documentElement.scrollHeight -
+          (event.target.documentElement.scrollTop + window.innerHeight) <
+        60
+      ) {
+        setPage((prev) => prev + 1);
+        getPage(page + 1);
+      }
+    },
+    [page, getPage],
+  );
 
   useEffect(() => {
-    if (loading) {
-      <p>Загружаем еще котиков</p>;
-      console.log('loading');
-    }
     document.addEventListener('scroll', handlerScroll);
     return function () {
       document.removeEventListener('scroll', handlerScroll);
     };
-  }, [loading]);
+  }, [handlerScroll]);
 
   return (
     <main>
@@ -51,7 +53,11 @@ export function MainPage() {
           );
         })}
       </List>
-      {loading && <p className="downloadMore">...загружаем еще котиков...</p>}
+      {loading && (
+        <p className="downloadMore">
+          ...загружаем {page > 0 ? 'еще' : ''} котиков...
+        </p>
+      )}
     </main>
   );
 }
