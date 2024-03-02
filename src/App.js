fc6df391b58ab76c0f137742d3007cd0cfe61c1f
Nataly <nz_18@mail.ru>
2024-03-02 21:12:32 +0300

    Feature/add adaptivity (#9)
    * feat: 🎸 add flavicon
    
    * refactor: 💡 add link to deploy to readme file
    
    * feat: 🎸 add adaptivity
    
    * refactor: 💡 alter some media quaries
    
    * refactor: 💡 change some media styles
    
    * refactor: 💡 connect normalize
    
    * refactor: 💡 correct the styles for adaptive layout
    
    * feat: 🎸 add state for mobile and touched devices
    
    * refactor: 💡 change some css styles
    
    * refactor: 💡 delete separeted initial state
    
    * feat: 🎸 add logic for mobile devices
    
    * refactor: 💡 correct styles in header and catcard component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './layout/Layout';
 import { FavoriteCatsPage } from './pages/favoriteCatsPage';
 import { MainPage } from './pages/mainPage';
+import { set } from './redux/slices/isTouchSlice';
 
 export function App() {
   const [page, setPage] = useState(0);
   const [data, setData] = useState([]);
+
   const getData = (dataFromApi) => setData(dataFromApi);
   const getPage = (pageCats) => setPage(pageCats);
+  const dispatch = useDispatch();
+  dispatch(
+    set(
+      navigator.maxTouchPoints > 0 ||
+        window.navigator.userAgent.toLocaleLowerCase().includes('mobi'),
+    ),
+  );
 
   return (
     <Routes>
