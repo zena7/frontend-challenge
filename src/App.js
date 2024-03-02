e159fa09309d984e9922fba6696a0f9bb41165b9
Nataly Zernova <nz_18@mail.ru>
2024-03-02 20:31:40 +0300

    feat: 🎸 add logic for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './layout/Layout';
 import { FavoriteCatsPage } from './pages/favoriteCatsPage';
 import { MainPage } from './pages/mainPage';
+import { set } from './redux/slices/isTouchSlice';
 
 export function App() {
   const [page, setPage] = useState(0);
   const [data, setData] = useState([]);
+
   const getData = (dataFromApi) => setData(dataFromApi);
   const getPage = (pageCats) => setPage(pageCats);
+  const dispatch = useDispatch();
+  dispatch(
+    set(
+      navigator.maxTouchPoints > 0 ||
+        window.navigator.userAgent.toLocaleLowerCase().includes('mobi'),
+    ),
+  );
+  const isTouch = useSelector((state) => state.touch.touch);
 
+  console.log(
+    'NAVIGATOR',
+    //   typeof window.navigator.userAgent,
+    window.navigator.userAgent,
+    navigator.maxTouchPoints,
+    navigator.maxTouchPoints > 0,
+    'или',
+    window.navigator.userAgent.toLocaleLowerCase().includes('mobi'),
+    isTouch,
+  );
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
--- src/catCard/CatCard.js
+++ src/catCard/CatCard.js
@@ -9,10 +9,12 @@ import { IconFavorite } from './assets/favoriteIcon';
 export function CatCard({ url }) {
   const favCats = useSelector((state) => state.likes.likes);
   const [like, setLike] = useState(url in favCats);
+  const isTouch = useSelector((state) => state.touch.touch);
   const dispatch = useDispatch();
 
   function handleClick() {
     setLike((prev) => !prev);
+
     if (url in favCats) {
       dispatch(deleteCard({ url }));
     } else {
@@ -22,7 +24,12 @@ export function CatCard({ url }) {
 
   return (
     <div className={classes.catContainer}>
-      <img src={url} alt="cat" className={classes.catImg} />
+      <img
+        src={url}
+        alt="cat"
+        className={classes.catImg}
+        onDoubleClick={handleClick}
+      />
       <Button
         className={classes.containerIconsFav}
         onClick={() => handleClick()}
@@ -31,8 +38,10 @@ export function CatCard({ url }) {
           className={`${classes.iconFavorite} ${like && classes.active}`}
         />
         <IconFavoriteBorder
-          className={`${classes.iconFavoriteBorder} ${like && classes.borderActive}`}
+          // className={`${classes.iconFavoriteBorder} ${like && !isTouch && classes.borderActive} ${isTouch && classes.borderActive}`}
+          className={`${classes.iconFavoriteBorder} ${like && classes.borderActive} ${isTouch && !like && classes.isTouch}`}
         />
+        {/* {isTouch && <Button>TOUCH</Button>} */}
       </Button>
     </div>
   );
