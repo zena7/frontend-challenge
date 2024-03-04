5d754fc36707eaa2fe6e5e9e426e0a2977fb72c9
Nataly Zernova <nz_18@mail.ru>
2024-03-04 20:11:30 +0300

    refactor: 💡 separate no cats paragraph

--- src/pages/favoriteCatsPage.js
+++ src/pages/favoriteCatsPage.js
@@ -1,20 +1,24 @@
 import { useSelector } from 'react-redux';
-import { List } from '../shared/ui/List';
 import { CatCard } from '../catCard/CatCard';
+import { List } from '../shared/ui/List';
 
 export function FavoriteCatsPage() {
   const favCats = useSelector((state) => state.likes.likes);
 
   return (
-    <List className="catsList">
-      {Object.values(favCats).length === 0 && <p>No favorite cats =(</p>}
-      {Object.values(favCats)?.map((item) => {
-        return (
-          <li key={item}>
-            <CatCard url={item} />
-          </li>
-        );
-      })}
-    </List>
+    <main>
+      {Object.values(favCats).length === 0 && (
+        <p className="noCats">No favorite cats</p>
+      )}
+      <List className="catsList">
+        {Object.values(favCats)?.map((item) => {
+          return (
+            <li key={item} className="catItem">
+              <CatCard url={item} />
+            </li>
+          );
+        })}
+      </List>
+    </main>
   );
 }
